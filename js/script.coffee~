eda806366c2d24ad824817df3d2d28ef3f1261c0
island205@dp <island205@gmail.com>
2012-03-22 17:00:35 +0800

    some test

---
+++ js/script.coffee~
@@ -0,0 +1,3 @@
+$ ->
+	width=$("body").css("witdh").split("px")[0]
+	$("#main").css("wdith",1*width*0.618)
--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-/* Author:
+(function() {
 
-*/
+  $(function() {
+    var width;
+    width = $("body").css("witdh").split("px")[0];
+    return $("#main").css("wdith", 1 * width * 0.618);
+  });
 
-
-
-
-
+}).call(this);
