3bc21f5bf27afc217d8a8d2b200542d420cf36a9
island205 <island205@gmail.com>
2012-03-27 00:29:32 +0800

    show content until domready

--- _site/js/script.coffee
+++ _site/js/script.coffee
@@ -1,3 +1,4 @@
 $ ->
-	width=$("body").css("witdh").split("px")[0]
-	$("#main").css("wdith",1*width*0.618)
+	width=$("body").css("width").split("px")[0]
+	$("#main").css("width",1*width*0.618)
+	$("body").css("visibility","visible")
--- _site/js/script.js
+++ _site/js/script.js
@@ -1,9 +1,8 @@
 (function() {
-
   $(function() {
     var width;
     width = $("body").css("width").split("px")[0];
-    return $("#main").css("width", 1 * width * 0.618);
+    $("#main").css("width", 1 * width * 0.618);
+    return $("body").css("visibility", "visible");
   });
-
 }).call(this);
